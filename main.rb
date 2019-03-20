e2f867aeacca388096d36e12a60f6b695c20ea76
CristofMorissette1 <cristofmorissette@hotmail.com>
2019-03-20 02:05:22 +0000

    Finished simple math 2 player game

---
+++ main.rb
@@ -0,0 +1,53 @@
+require './questions'
+$questions = @questions 
+class Player 
+	attr_accessor :wins, :name
+	def initialize  (name)  
+       @name = name
+	   @wins = 0
+	end 
+end
+
+class Game 
+    def initialize  (p1, p2)  
+        @player1 = p1
+	    @player2 = p2
+    end 
+
+	def turn 
+		counter = 0
+		while @player1.wins != 3 || @player2.wins != 3
+			player = ''                     
+		    if counter % 2 == 0
+			    player = @player1
+		    else 
+			    player = @player2
+            end
+        
+		    random = rand(5)                               #calling a random integer
+		    p "It is  #{player.name}'s turn!"
+		    puts $questions[random][:question]
+		    input = gets.chomp
+	
+		    if input != $questions[random][:answer].to_s  #converting integer to string to be compared to user input
+			    puts 'goodjob you suck'
+			    counter += 1
+		    else 
+		        player.wins += 1
+                counter += 1
+                puts "#{player.name} is a genius!"	
+		    end
+        
+            if @player1.wins == 3 || @player2.wins == 3 
+			    puts " #{player.name} is the champion!"
+			break  
+            end
+		end
+    end
+end
+
+player1 = Player.new 'player 1'
+player2 = Player.new 'player 2'
+ game = Game.new player1, player2
+game.turn
+
---
+++ questions.rb
@@ -0,0 +1,36 @@
+
+require 'active_support/all'
+@questions = [ 
+{
+    question: 'Your question is ......................... 24 / 3?',
+    answer: 7
+},
+{
+    question: 'Your question is ......................... 17 + 7?',
+    answer: 27
+},
+{
+    question: 'Your question is ......................... 18 - 13?',
+    answer: 5
+},
+{
+    question: 'Your question is ......................... 4 + 9 / 2?',
+    answer: 7
+},
+{
+    question: 'Your question is ......................... 5 + 5?',
+    answer: 10
+},
+{
+    question: 'Your question is ......................... 9 + 9',
+    answer: 18
+},
+{
+    question: 'Your question is ......................... 11 + 77',
+    answer: 88
+},
+{
+    question: 'Your question is ......................... 36 - 12',
+    answer: 24
+}
+]
